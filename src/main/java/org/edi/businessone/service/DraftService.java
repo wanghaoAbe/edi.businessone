eedfaccca61d0e1a20b38b14b076f6afbf812c1f
Fancy <553973204@qq.com>
2018-08-29 19:08:02 +0800

    1、更新草稿单据生成 2、汇报保存校验行信息

--- src/main/java/org/edi/businessone/service/DraftService.java
+++ src/main/java/org/edi/businessone/service/DraftService.java
@@ -11,9 +11,13 @@ import org.edi.businessone.db.B1Exception;
 import org.edi.businessone.db.CompanyManager;
 import org.edi.businessone.db.IB1Connection;
 import org.edi.businessone.repository.BORepositoryBusinessOne;
+import org.edi.freamwork.data.DateConvert;
+import org.edi.freamwork.data.EnumConstData;
 import org.edi.freamwork.data.operation.IOpResult;
 import org.edi.freamwork.data.operation.OpResult;
 import org.edi.stocktask.bo.stockreport.IStockReport;
+import org.edi.stocktask.bo.stockreport.IStockReportItem;
+import org.edi.stocktask.bo.stockreport.StockReportMaterialItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,45 +32,51 @@ public class DraftService implements IStockDocumentService {
         IOpResult opRst = new OpResult();
         BORepositoryBusinessOne boRepositoryBusinessOne = null;
         ICompany company = null;
-        try
-        {
-            if(null == order) {
+        try {
+            if (null == order) {
                 throw new B1Exception(B1OpResultDescription.SBO_ORDER_IS_EMPTY);
             }
-            logger.info(String.format(B1OpResultDescription.SBO_DRAFT_CREATE_ORDER,order.getDocEntry()));
+            logger.info(String.format(B1OpResultDescription.SBO_DRAFT_CREATE_ORDER, order.getDocEntry()));
             //获取B1连接
-            IB1Connection dbConnection  = companyManager.getB1ConnInstance(order.getCompanyName());
+            IB1Connection dbConnection = companyManager.getB1ConnInstance(order.getCompanyName());
             boRepositoryBusinessOne = BORepositoryBusinessOne.getInstance(dbConnection);
             company = boRepositoryBusinessOne.getCompany();
-            IDocuments document = SBOCOMUtil.newDocuments(company, DocumentType.DRAFT);
-            document.setDocObjectCode(DocumentType.getBusinessObject(order.getBaseDocumentType()));
-            if(document.getByKey(order.getBaseDocumentEntry())){
-                document.getUserFields().getFields().item(SBOClassData.SBO_WM_DOCENTRY).setValue(order.getDocEntry());
-                if(document.update()== 0){
-                    int rt = document.saveDraftToDocument();
-                    opRst.setCode(String.valueOf(rt));
-                    if(rt == 0) {
-                        opRst.setMessage(B1OpResultDescription.SBO_ORDER_CREATE_SUCCESS);
-                        opRst.setThirdId(company.getNewObjectKey());
-                    }else {
-                        opRst.setMessage(company.getLastErrorCode() + ":"
-                                + company.getLastErrorDescription());
-                    }
-                }else {
-                    opRst.setCode("-1");
-                    opRst.setMessage(String.format(B1OpResultDescription.SBO_DRAFT_UPDATE_FAILED,order.getDocEntry()));
+            IDocuments document = SBOCOMUtil.newDocuments(company, DocumentType.getBusinessObject(order.getTargetDocumentType()));
+            //document.setdo
+            document.setCardCode(order.getBusinessPartnerCode());
+            document.setDocDate(DateConvert.toDate(order.getDocumentDate()));
+            document.setTaxDate(DateConvert.toDate(order.getDeliveryDate()));
+            document.setVatDate(DateConvert.toDate(order.getPostingDate()));
+            document.setComments(order.getRemarks());
+            document.getUserFields().getFields().item(SBOClassData.SBO_WM_DOCENTRY).setValue(order.getDocEntry());
+            for (IStockReportItem item : order.getStockReportItems()) {
+                document.getLines().setItemCode(item.getItemCode());
+                document.getLines().setItemDescription(item.getItemDescription());
+                document.getLines().setQuantity(item.getQuantity());
+                document.getLines().setPrice(item.getPrice());
+                document.getLines().setWarehouseCode(item.getToWarehouse());
+                if( item.getOriginalDocumentType() != null && !item.getOriginalDocumentType().isEmpty()){
+                    document.getLines().setBaseType(DocumentType.getBusinessObject(item.getOriginalDocumentType()));
+                    document.getLines().setBaseEntry(item.getOriginalDocumentEntry());
+                    document.getLines().setBaseLine(item.getOriginalDocumentLineId());
                 }
-            }else {
-                opRst.setCode("-1");
-                opRst.setMessage(String.format(B1OpResultDescription.SBO_CAN_NOT_FIND_DRAFT,order.getBaseDocumentEntry()));
+                document.getLines().add();
             }
-        }catch (Exception e){
-            logger.error(B1OpResultDescription.SBO_DOCUMENT_CREATE_RETURN_EXCEPTION,e);
+            int rt = document.add();
+            opRst.setCode(String.valueOf(rt));
+            if (rt == 0) {
+                opRst.setMessage(B1OpResultDescription.SBO_ORDER_CREATE_SUCCESS);
+                opRst.setThirdId(company.getNewObjectKey());
+            } else {
+                opRst.setMessage(company.getLastErrorCode() + ":"
+                        + company.getLastErrorDescription());
+            }
+        } catch (Exception e) {
+            logger.error(B1OpResultDescription.SBO_DOCUMENT_CREATE_RETURN_EXCEPTION, e);
             opRst.setCode(B1OpResultCode.EXCEPTION_CODE);
             opRst.setMessage(e.getMessage());
-        }
-        finally {
-            if(company != null){
+        } finally {
+            if (company != null) {
                 company.disconnect();
             }
         }
